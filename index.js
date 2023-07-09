3a82ec32187b1fb0a1d5693c5165b43676d1c6f8
amitpareek <amit@cakewalk.in>
2023-07-09 13:38:13 +0000

    gitpod

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get("/health", (req, res) => {
 });
 
 // start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
